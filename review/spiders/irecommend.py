4c4dfad93bb33c9658462f63bdef6a5d54f5be47
Aleksey Kuritsyn <bimastics@gmail.com>
2022-05-13 06:49:00 +0700

    Initialize the spider

---
+++ review/spiders/irecommend.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class IrecommendSpider(scrapy.Spider):
+    name = 'irecommend'
+    allowed_domains = ['irecommend.ru']
+    start_urls = ['http://irecommend.ru/']
+
+    def parse(self, response):
+        pass
