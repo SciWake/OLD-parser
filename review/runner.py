81be1e463d6b277896693f8513299f5884d80157
Aleksey Kuritsyn <bimastics@gmail.com>
2022-05-13 14:46:26 +0700

    Update runner

--- review/runner.py
+++ review/runner.py
@@ -1,11 +1,11 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from review import settings
-from review.spiders.irecommend import IrecommendSpider
+from review.spiders.otzovik import OtzovikSpider
 
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(IrecommendSpider)
+    crawler_process.crawl(OtzovikSpider)
     crawler_process.start()
