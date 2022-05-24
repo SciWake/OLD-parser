8837a58442334bfbf4c42a3500e41f876156a425
Aleksey Kuritsyn <bimastics@gmail.com>
2022-05-24 16:44:27 +0700

    The ability to skip the passed pages

--- review/spiders/otzovik.py
+++ review/spiders/otzovik.py
@@ -6,10 +6,22 @@ from review.items import ReviewItem
 from scrapy.loader import ItemLoader
 
 
+def read_pickle(file):
+    with open(os.path.join(os.getcwd(), file), 'rb') as f:
+        return pickle.load(f)
+
+
+def write_pickle(urls, file):
+    with open(os.path.join(os.getcwd(), file), 'wb') as f:
+        return pickle.dump(urls, f)
+
+
 class OtzovikSpider(scrapy.Spider):
     name = 'otzovik'
     allowed_domains = ['otzovik.com']
-    start_urls = ['https://otzovik.com/health/fragrance']
+    start_urls = ['https://otzovik.com/health/caring_cosmetics/?order=rate']
+    pagination_product = read_pickle('pagination_product.pickle')
+    product = read_pickle('product.pickle')
 
     with open(os.path.join(os.getcwd(), 'database.csv'), 'r', newline='', encoding='UTF-8') as csv_file:
         id_to_csv = [row['id'] for row in csv.DictReader(csv_file)]
@@ -22,23 +34,31 @@ class OtzovikSpider(scrapy.Spider):
 
     def parse(self, response):
         # Getting base pagination links
-        for page_url in response.css('div.pager a.nth::attr("href")'):
+        for page_url in response.css('div.pager a.next::attr("href")'):
             yield response.follow(page_url, callback=self.parse)
 
         # Getting links to product
         for product_url in response.css('div.product-list tr a.product-name::attr("href")'):
+            if product_url.extract() in self.product:  # Повторный обход
+                continue
             yield response.follow(product_url, callback=self.parse_product)
 
-    def parse_product(self, response):
+    def parse_product(self, response, referer=None):
         # Getting product pagination links
-        for prod_page_url in response.css('div.pager a.nth::attr("href")'):
-            yield response.follow(prod_page_url, callback=self.parse_product)
+        for prod_page_url in response.css('div.pager a.next::attr("href")'):
+            # if prod_page_url.extract() in self.pagination_product:  # Повторный обход
+            #     continue
+            yield response.follow(prod_page_url, callback=self.parse_product, cb_kwargs=dict(referer=response.url))
 
         # Getting links to review
         for review_url in response.css('div.review-list-chunk div.mshow0 a.review-read-link::attr("href")'):
-            if 'https://otzovik.com' + review_url.extract() in self.id_to_csv:  # Повторный обход
+            if review_url.extract() in self.id_to_csv:  # Повторный обход
                 continue
             yield response.follow(review_url, callback=self.parse_review)
+
+        if referer and referer.replace('https://otzovik.com', '') not in self.product:
+            self.product.add(referer.replace('https://otzovik.com', ''))
+            write_pickle(self.product, 'product.pickle')
 
     def parse_review(self, response):
         item = ItemLoader(ReviewItem(), response)
@@ -57,4 +77,11 @@ class OtzovikSpider(scrapy.Spider):
         item.add_css('user_login', 'div.review-header div.login-col span::text')
         item.add_css('user_karma', 'div.review-header div.karma-col div.karma::text')
         item.add_css('reviews_count', 'div.review-header div.reviews-col a.reviews-counter::text')
+
+        # Сохранение продуктов, которые нет необходимости обходить повторно
+        count = response.css('div.product-header-button-wrap span.reviews-counter a::text').extract()[0].split()[-1]
+        if int(count) <= 20:
+            self.product.add(response.css('div.product-header a.product-name::attr("href")').extract()[0])
+            write_pickle(self.product, 'product.pickle')
+
         yield item.load_item()
